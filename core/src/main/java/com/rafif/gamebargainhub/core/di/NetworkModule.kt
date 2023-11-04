5f120689110765f0c638289f08322e3629909fe3
Muhammad Rafif Baihaqi <raphstonks@gmail.com>
2023-11-04 19:55:31 +0700

    refactor: add certificate pinning

--- core/src/main/java/com/rafif/gamebargainhub/core/di/NetworkModule.kt
+++ core/src/main/java/com/rafif/gamebargainhub/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -17,10 +18,17 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val hostname = "www.cheapshark.com"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/j4Y3d739JotaAgRcIUZMgkDyi9X4gkKvw2TAp+z36L0=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .add(hostname, "sha256/Y9mvm0exBk1JoQ57f9Vm28jKo5lFm/woKcVxrYxu80o=")
+            .build()
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
